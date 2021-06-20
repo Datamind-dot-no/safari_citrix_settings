c7e106f92b6f785f4cdc1af46a51d12eb9f06e79
Martinus Verburg <12513894+codeskipper@users.noreply.github.com>
2021-06-20 19:36:22 +0200

    first commit - working OK using Python 2

---
+++ munki_postinstall.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+###########################      About this script      ##########################
+#                                                                                #
+#   safari_citrix_settings.py - (Munki)postinstall script                        #                                  #
+#                                                                                #
+#   Purpose:                                                                     #
+#           call safari_citrix_settings.py script as console user                #
+#            *** with full disk access ***                                       #
+#                                                                                #
+#   Created by Martinus Verburg                                                  #
+#               March 2021                                                       #
+#                                                                                #
+#   Changelog                                                                    #
+#            2021-06-14 - moved away from munkipkg postinstall as apparently     #
+#                         the Installer app doesn't have full disk access while  #
+#                         WS1-Munki does run it successfully as                  #
+#                         postinstall script in the pkginfo                      #
+#                                                                                #
+#                                                                                #
+#   Instructions                                                                 #
+#            designed to run by software management application                  #
+#            such as VMware Workspace ONE, Munki                                 #
+#            MUST be run from process entitled to full disk access in order to   #
+#            write to sandboxed Safari settings                                  #
+#            in ~/Library/Containers/com.apple.Safari                            #
+#                                                                                #
+##################################################################################
+
+# Wait until Finder is running to ensure a console user is logged in
+while ! pgrep -q Finder ; do
+		echo "Waiting until console user is logged in"
+		sleep 10
+done
+
+# get Console user
+# Thanks to Graham Pugh - copied from erase-install.sh
+current_user=$(/usr/sbin/scutil <<< "show State:/Users/ConsoleUser" | /usr/bin/awk -F': ' '/[[:space:]]+Name[[:space:]]:/ { if ( $2 != "loginwindow" ) { print $2 }}')
+
+sudo -u $current_user /Library/Management/safari_citrix_settings.py
---
+++ payload/Library/Management/safari_citrix_settings.py
@@ -0,0 +1,124 @@
+#!/usr/bin/python
+
+'''
+safari_citrix_settings.py
+
+Purpose:
+    ensure .ica is in (Safari) com.apple.DownloadAssessment download Safe category extensions,
+    ensure .ica is not in (Safari) com.apple.DownloadAssessment other category extensions
+    ensure Open safe downloads is enabled in com.apple.Safari (as is default)
+    ensure Citrix Workspace autoupdate is disabled
+
+
+How:
+    Must run in user context, with user logged in at console.
+    Uses CoreFoundation PyObjC CoreFoundation to support cached prefs and to avoid need for app restart
+    Does not lock down settings like a configuration profile would, suitable for e.g. run-once to provide initial settings
+    Does not overwrite existing category lists of safe extensions, strives to be omnipotent
+
+Caveats:
+    In order to ensure Open Safe Downloads is enabled, this script needs to be run from an
+    executable with full disk access because on recent systems Safari prefs are sandboxed
+    and SIP is enabled by default.
+
+ToDo:
+    No check yet if prefs were actually stored, future changes like sandboxing might cause issue.
+
+Thanks to:
+    https://www.blackmanticore.com/1c569206754935dacb0dc6b89ca818b8
+    https://gist.github.com/gregneagle/010b369e86410a2f279ff8e980585c68
+    https://gist.github.com/gregneagle/01c99322cf985e771827
+    https://lapcatsoftware.com/articles/containers.html
+'''
+
+# import plistlib
+import os.path
+import sys
+import CoreFoundation
+from Foundation import NSMutableArray, NSMutableDictionary, NSHomeDirectory
+
+def setDownloadAssessmentCategory(extension, risk_category):
+    risk_categories = { 'LSRiskCategorySafe', 'LSRiskCategoryNeutral', 'LSRiskCategoryUnsafeExecutable', 'LSRiskCategoryMayContainUnsafeExecutable' }
+    for arisk_category in risk_categories:
+        # read the dict with current category of safe extensions - gets an immutable return object
+        cur_risk_category = CoreFoundation.CFPreferencesCopyAppValue( arisk_category, "com.apple.DownloadAssessment")
+
+        if cur_risk_category:
+            # copy immutable dict to new mutable one
+            my_risk_category = NSMutableDictionary.alloc().initWithDictionary_copyItems_(cur_risk_category, True)
+        else:
+            # create an empty dict
+            my_risk_category = {}
+
+        if 'LSRiskCategoryExtensions' in my_risk_category:
+            cur_risk_exts = my_risk_category['LSRiskCategoryExtensions']
+            my_risk_category['LSRiskCategoryExtensions'] = NSMutableArray.alloc().initWithArray_(cur_risk_exts)
+        else:
+            # create an empty array for the extensions
+            my_risk_category['LSRiskCategoryExtensions'] = []
+
+        if arisk_category == risk_category:
+            if not extension in my_risk_category['LSRiskCategoryExtensions']:
+                my_risk_category['LSRiskCategoryExtensions'].append(extension)
+                print 'Adding extension "%s" to risk category array "%s" of com.apple.DownloadAssessment' % (extension, risk_category)
+            else:
+                print 'extension "%s" is already present in risk category array "%s" of com.apple.DownloadAssessment' % (extension, risk_category)
+        else:
+            # ensure the_extension is NOT in any of the remaining categories
+            if extension in my_risk_category['LSRiskCategoryExtensions']:
+                my_risk_category['LSRiskCategoryExtensions'].remove(extension)
+                print 'Removing extension "%s" from risk category array "%s" of com.apple.DownloadAssessment' % (extension, arisk_category)
+            # save the changed preference
+        CoreFoundation.CFPreferencesSetAppValue(arisk_category, my_risk_category,  "com.apple.DownloadAssessment")
+    CoreFoundation.CFPreferencesAppSynchronize("com.apple.DownloadAssessment")
+
+
+
+def main():
+    # ensure .ica is in Safari category for Safe download extensions
+    setDownloadAssessmentCategory('ica', 'LSRiskCategorySafe')
+    # for testing:
+    #setDownloadAssessmentCategory('zip', 'LSRiskCategoryNeutral')
+
+    # ensure Citrix Workspace AutoUpdate is set to manual
+    cur_citrix_autoupdate = CoreFoundation.CFPreferencesCopyAppValue( "AutoUpdateState", "com.citrix.receiver.nomas" )
+    if not cur_citrix_autoupdate == "Manual":
+        CoreFoundation.CFPreferencesSetAppValue( "AutoUpdateState", "Manual", "com.citrix.receiver.nomas" )
+        print 'Citrix preference com.citrix.receiver.nomas AutoUpdateState set to Manual'
+    else:
+        print 'Citrix preference com.citrix.receiver.nomas AutoUpdateState already set to Manual'
+
+    # ensure AutoOpenSafeDownloads is on for com.apple.Safari
+    # test if Safari version is Sandboxed by checking for container
+    # must attempt to write to sandboxed plist to ensure new setting does not end up in old prefs path as ghost plist
+    homeDirectory = NSHomeDirectory()
+    if os.path.isdir(homeDirectory + "/Library/Containers/com.apple.Safari"):
+        app_ID = homeDirectory + "/Library/Containers/com.apple.Safari/Data/Library/Preferences/com.apple.Safari"
+        safari_prefs_path = app_ID + ".plist"
+    else:
+        # must be old version - not sandboxed yet
+        app_ID = 'com.apple.Safari'
+        safari_prefs_path = homeDirectory + "/Library/Preferences/" + app_ID + ".plist"
+        cur_open_safe_downloads = CoreFoundation.CFPreferencesCopyAppValue( "AutoOpenSafeDownloads", app_ID )
+    if cur_open_safe_downloads is None:
+        print app_ID + ' AutoOpenSafeDownloads key is not present, it defaults to True'
+    else:
+        if not cur_open_safe_downloads:
+            print 'Open safe Downloads is not enabled in ' + safari_prefs_path
+            # test if script has access, SIP will limit access to container contents, must run from entitled app with full disk access
+            if os.access(safari_prefs_path, os.W_OK):
+                print 'Access OK to Safari prefs at' + safari_prefs_path
+            else:
+                sys.exit('No access to Safari prefs at' + safari_prefs_path + ' - bailing out')
+            CoreFoundation.CFPreferencesSetAppValue( "AutoOpenSafeDownloads", True, app_ID )
+            CoreFoundation.CFPreferencesAppSynchronize( app_ID )
+            print app_ID + ' AutoOpenSafeDownloads now set to true'
+        else:
+            print 'Open safe Downloads already enabled in ' + safari_prefs_path
+
+    sys.exit(0)
+
+
+
+if __name__ == '__main__':
+    main()
